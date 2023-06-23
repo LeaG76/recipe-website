60fb5d32028dc467e6f05600fe51d2681fb8c0fa
Léa Gallier <100634711+LeaG76@users.noreply.github.com>
2023-06-23 11:22:30 +0200

    Update sliders and homepage

--- public/javascripts/swiper.js
+++ public/javascripts/swiper.js
@@ -16,20 +16,20 @@ var swiperCategories = new Swiper(".category-slide-container", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    374: {
-      slidesPerView: 2,
-    },
-    1050: {
-      slidesPerView: 3,
-    },
-    1500: {
-      slidesPerView: 4,
-    }
-  },
+  // breakpoints: {
+  //   0: {
+  //     slidesPerView: 1,
+  //   },
+  //   374: {
+  //     slidesPerView: 2,
+  //   },
+  //   1050: {
+  //     slidesPerView: 3,
+  //   },
+  //   1500: {
+  //     slidesPerView: 4,
+  //   }
+  // },
 });
 
 /* Slider recipes */
