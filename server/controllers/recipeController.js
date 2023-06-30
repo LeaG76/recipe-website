6e1aa0f241dd053e42b0e417132a658e527f987c
Léa Gallier <100634711+LeaG76@users.noreply.github.com>
2023-06-30 16:22:50 +0200

    Import schema

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,5 +1,6 @@
 require('../models/database');
 const Category = require('../models/Category');
+const Recipe = require('../models/Recipe');
 
 const aboutPictures = require('../../data/about-pictures.json');
 
